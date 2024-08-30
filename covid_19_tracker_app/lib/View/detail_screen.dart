b2127b3acc00e6616021e54e94a3af3b758174b5
nitavantaniit <133080427+nitavantaniit@users.noreply.github.com>
2024-08-30 08:23:17 +0530

    first commit

---
+++ covid_19_tracker_app/lib/View/detail_screen.dart
@@ -0,0 +1,65 @@
+import 'package:covid_19_tracker_app/View/world_stats.dart';
+import 'package:flutter/material.dart';
+
+class DetailsScreen extends StatefulWidget {
+
+  String image;
+  String name;
+  int totalCases, totalDeaths, totalRecovered, active, critical, todayRecovered, test;
+  DetailsScreen({
+    required this.image,
+    required this.name,
+    required this.totalCases,
+    required this.totalDeaths,
+    required this.totalRecovered,
+    required this.active,
+    required this.critical,
+    required this.todayRecovered,
+    required this.test,
+  });
+
+  @override
+  State<DetailsScreen> createState() => _DetailsScreenState();
+}
+
+class _DetailsScreenState extends State<DetailsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.name),
+        centerTitle: true,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Stack(
+            alignment: Alignment.topCenter,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(top: MediaQuery.of(context).size.height * .067),
+                child: Card(
+                  child: Column(
+                    children: [
+                      SizedBox(height: MediaQuery.of(context).size.height * .06,),
+                      ReusableRow(title: 'Cases', value: widget.totalCases.toString()),
+                      ReusableRow(title: 'Recovered', value: widget.totalRecovered.toString()),
+                      ReusableRow(title: 'Death', value: widget.totalDeaths.toString()),
+                      ReusableRow(title: 'Critical', value: widget.critical.toString()),
+                      ReusableRow(title: 'Today Recovered', value: widget.totalRecovered.toString()),
+                    ],
+                  ),
+                ),
+              ),
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: NetworkImage(widget.image),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
